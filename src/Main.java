19eb0bb668d5b98b64979dd81197ccf46cace841
Jacu <36055042+JacekFlak@users.noreply.github.com>
2018-04-16 21:57:33 +0200

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import javax.swing.JFrame;
+public class Main {
+public static void  main(String[] args )
+{
+JFrame objekt = new JFrame();
+objekt.setBounds(10,10,700,600);
+objekt.setTitle("Moja pierwsza gra");
+objekt.setResizable(false);
+objekt.setVisible(true);
+objekt.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+
+}
+
+
+
+
+}
